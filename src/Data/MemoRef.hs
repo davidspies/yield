581633a56a46e92b6d5b40af9eafa101f0784f93
David Spies <dnspies@gmail.com>
2019-03-14 22:45:25 -0700

    Document MemoRef

--- src/Data/MemoRef.hs
+++ src/Data/MemoRef.hs
@@ -11,12 +11,21 @@ import           DSpies.Prelude
 import           Control.Concurrent.STM
 import           Control.Monad.Fix              ( mfix )
 
+-- |
+-- A box which holds an IO action and lazily evaluates it only if and when the
+-- value is needed.
 newtype MemoRef a = MemoRef (TVar (IO a))
 
--- | Equivalent to @ newMemoRef . return @
+-- |
+-- Equivalent to
+-- @ 'newMemoRef' . 'return' @
 pureMemoRef :: a -> IO (MemoRef a)
 pureMemoRef = fmap MemoRef . newTVarIO . return
 
+-- |
+-- Will execute the action at most once when and if 'readMemoRef' is called
+-- on the returned 'MemoRef'.
+-- Subsequent calls to 'readMemoRef' just return the memoized result.
 newMemoRef :: IO a -> IO (MemoRef a)
 newMemoRef act = fmap MemoRef $ do
   begunEvaluation <- newTVarIO False
@@ -35,5 +44,11 @@ newMemoRef act = fmap MemoRef $ do
           writeTVar r (return result)
         return result
 
+-- |
+-- Read from a 'MemoRef' (see 'newMemoRef')
+--
+-- Note: This function is thread-safe. If multiple threads try
+-- to read from a 'MemoRef' simultaneously, all but the first will block until
+-- the action finishes (and then return the memoized result).
 readMemoRef :: MemoRef a -> IO a
 readMemoRef (MemoRef actRef) = join $ readTVarIO actRef
